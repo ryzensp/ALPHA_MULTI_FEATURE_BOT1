17d4c271e5cb502e77cf9058b485af85bf5c7c7c
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-03 14:48:13 +0530

    Update acutebot.config.py

--- acutebot/acutebot.config.py
+++ acutebot/acutebot.config.py
@@ -17,13 +17,16 @@ import os, sys, logging
 from functools import wraps
 from telegram.ext import Updater, Defaults
 
+    class Config(object):
     TOKEN = os.environ.get("TOKEN")
     WORKERS = int(os.environ.get("WORKERS", 8))
     TMDBAPI = os.environ.get("TMDBAPI")
     DB_URI = os.environ.get("DATABASE_URL")
     GENIUS = os.environ.get("GENIUS")
-    DEBUG = bool(os.environ.get("DEBUG", False)) 
+    SPT_CLIENT_SECRET = os.environ.get("SPT_CLIENT_SECRET")
+    SPT_CLIENT_ID = os.environ.get("SPT_CLIENT_ID")
+    DEBUG = bool(os.environ.get("DEBUG", False))
+    ARLTOKEN = os.environ.get("ARL")
+    APP_URL = os.environ.get("APP_URL")
     APIID = os.environ.get("APIID")
     APIHASH = os.environ.get("APIHASH")
-
-
