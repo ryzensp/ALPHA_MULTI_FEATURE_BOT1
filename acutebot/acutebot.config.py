f4d4c50af03299afb12345ca05bdb45eeed6e7db
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-03 14:58:16 +0530

    Update acutebot.config.py

--- acutebot/acutebot.config.py
+++ acutebot/acutebot.config.py
@@ -1,2 +1,11 @@
 else:
      import Config
+
+    TOKEN = Config.TOKEN
+    WORKERS = Config.WORKERS
+    TMDBAPI = Config.TMDBAPI
+    DB_URI = Config.DB_URI
+    GENIUS = Config.GENIUS    
+    DEBUG = Config.DEBUG    
+    APIID = Config.APIID
+    APIHASH = Config.APIHASH
