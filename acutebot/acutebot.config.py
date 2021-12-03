de657c8f2fca4c7167b0d4d373ee39bbfd5f1225
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-03 14:56:42 +0530

    Update acutebot.config.py

--- acutebot/acutebot.config.py
+++ acutebot/acutebot.config.py
@@ -1,25 +1,2 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# MIT License
-# Copyright (c) 2020 Stɑrry Shivɑm // This file is part of AcuteBot
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
-import os, sys, logging
-
-    TOKEN = os.environ.get("TOKEN")
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    TMDBAPI = os.environ.get("TMDBAPI")
-    DB_URI = os.environ.get("DATABASE_URL")
-    GENIUS = os.environ.get("GENIUS")    
-    DEBUG = bool(os.environ.get("DEBUG", False))    
-    APIID = os.environ.get("APIID")
-    APIHASH = os.environ.get("APIHASH")
+else:
+     import Config
